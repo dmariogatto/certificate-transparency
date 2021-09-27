11088e09d9d87a19bb49690bc8685099a9005bff
dmariogatto <d.mario.gatto@gmail.com>
2021-09-27 17:33:26 +0930

    Update Resource.designer.cs

--- src/Tests/Tests.Droid/Resources/Resource.designer.cs
+++ src/Tests/Tests.Droid/Resources/Resource.designer.cs
@@ -25,6 +25,7 @@ namespace Tests.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Android.NUnitLite.Resource.Id.CreditsButton = global::Tests.Droid.Resource.Id.CreditsButton;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionHostName = global::Tests.Droid.Resource.Id.OptionHostName;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionPort = global::Tests.Droid.Resource.Id.OptionPort;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionRemoteServer = global::Tests.Droid.Resource.Id.OptionRemoteServer;
@@ -41,10 +42,12 @@ namespace Tests.Droid
 			global::Xamarin.Android.NUnitLite.Resource.Id.ResultsPassed = global::Tests.Droid.Resource.Id.ResultsPassed;
 			global::Xamarin.Android.NUnitLite.Resource.Id.ResultsResult = global::Tests.Droid.Resource.Id.ResultsResult;
 			global::Xamarin.Android.NUnitLite.Resource.Id.ResultStackTrace = global::Tests.Droid.Resource.Id.ResultStackTrace;
+			global::Xamarin.Android.NUnitLite.Resource.Id.RunEverythingButton = global::Tests.Droid.Resource.Id.RunEverythingButton;
 			global::Xamarin.Android.NUnitLite.Resource.Id.RunTestsButton = global::Tests.Droid.Resource.Id.RunTestsButton;
 			global::Xamarin.Android.NUnitLite.Resource.Id.TestSuiteListView = global::Tests.Droid.Resource.Id.TestSuiteListView;
 			global::Xamarin.Android.NUnitLite.Resource.Layout.options = global::Tests.Droid.Resource.Layout.options;
 			global::Xamarin.Android.NUnitLite.Resource.Layout.results = global::Tests.Droid.Resource.Layout.results;
+			global::Xamarin.Android.NUnitLite.Resource.Layout.Runner = global::Tests.Droid.Resource.Layout.Runner;
 			global::Xamarin.Android.NUnitLite.Resource.Layout.test_result = global::Tests.Droid.Resource.Layout.test_result;
 			global::Xamarin.Android.NUnitLite.Resource.Layout.test_suite = global::Tests.Droid.Resource.Layout.test_suite;
 		}
@@ -66,58 +69,64 @@ namespace Tests.Droid
 		{
 			
 			// aapt resource value: 0x7F010000
-			public const int OptionHostName = 2130771968;
+			public const int CreditsButton = 2130771968;
 			
 			// aapt resource value: 0x7F010001
-			public const int OptionPort = 2130771969;
+			public const int OptionHostName = 2130771969;
 			
 			// aapt resource value: 0x7F010002
-			public const int OptionRemoteServer = 2130771970;
+			public const int OptionPort = 2130771970;
 			
 			// aapt resource value: 0x7F010003
-			public const int OptionsButton = 2130771971;
+			public const int OptionRemoteServer = 2130771971;
 			
 			// aapt resource value: 0x7F010004
-			public const int ResultFullName = 2130771972;
+			public const int OptionsButton = 2130771972;
 			
 			// aapt resource value: 0x7F010005
-			public const int ResultMessage = 2130771973;
+			public const int ResultFullName = 2130771973;
 			
 			// aapt resource value: 0x7F010006
-			public const int ResultResultState = 2130771974;
+			public const int ResultMessage = 2130771974;
 			
 			// aapt resource value: 0x7F010007
-			public const int ResultRunSingleMethodTest = 2130771975;
-			
-			// aapt resource value: 0x7F010009
-			public const int ResultsFailed = 2130771977;
-			
-			// aapt resource value: 0x7F01000A
-			public const int ResultsId = 2130771978;
-			
-			// aapt resource value: 0x7F01000B
-			public const int ResultsIgnored = 2130771979;
-			
-			// aapt resource value: 0x7F01000C
-			public const int ResultsInconclusive = 2130771980;
-			
-			// aapt resource value: 0x7F01000D
-			public const int ResultsMessage = 2130771981;
-			
-			// aapt resource value: 0x7F01000E
-			public const int ResultsPassed = 2130771982;
-			
-			// aapt resource value: 0x7F01000F
-			public const int ResultsResult = 2130771983;
+			public const int ResultResultState = 2130771975;
 			
 			// aapt resource value: 0x7F010008
-			public const int ResultStackTrace = 2130771976;
+			public const int ResultRunSingleMethodTest = 2130771976;
+			
+			// aapt resource value: 0x7F01000A
+			public const int ResultsFailed = 2130771978;
+			
+			// aapt resource value: 0x7F01000B
+			public const int ResultsId = 2130771979;
+			
+			// aapt resource value: 0x7F01000C
+			public const int ResultsIgnored = 2130771980;
+			
+			// aapt resource value: 0x7F01000D
+			public const int ResultsInconclusive = 2130771981;
+			
+			// aapt resource value: 0x7F01000E
+			public const int ResultsMessage = 2130771982;
+			
+			// aapt resource value: 0x7F01000F
+			public const int ResultsPassed = 2130771983;
 			
 			// aapt resource value: 0x7F010010
-			public const int RunTestsButton = 2130771984;
+			public const int ResultsResult = 2130771984;
+			
+			// aapt resource value: 0x7F010009
+			public const int ResultStackTrace = 2130771977;
 			
 			// aapt resource value: 0x7F010011
-			public const int TestSuiteListView = 2130771985;
+			public const int RunEverythingButton = 2130771985;
+			
+			// aapt resource value: 0x7F010012
+			public const int RunTestsButton = 2130771986;
+			
+			// aapt resource value: 0x7F010013
+			public const int TestSuiteListView = 2130771987;
 			
 			static Id()
 			{
@@ -139,10 +148,13 @@ namespace Tests.Droid
 			public const int results = 2130837505;
 			
 			// aapt resource value: 0x7F020002
-			public const int test_result = 2130837506;
+			public const int Runner = 2130837506;
 			
 			// aapt resource value: 0x7F020003
-			public const int test_suite = 2130837507;
+			public const int test_result = 2130837507;
+			
+			// aapt resource value: 0x7F020004
+			public const int test_suite = 2130837508;
 			
 			static Layout()
 			{
